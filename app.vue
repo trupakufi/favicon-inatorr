6fd4a15461740cffc2a364eeba62a6deac360558
trupakufi - Mário Varela <trupakufi@gmail.com>
2024-09-21 17:46:28 +0100

    seo: add configs for adsense

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4117999514952023",
+      crossorigin: "anonymous",
+      tagPosition: "head",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Favicon Ganerator</h1>
   </div>
 </template>
